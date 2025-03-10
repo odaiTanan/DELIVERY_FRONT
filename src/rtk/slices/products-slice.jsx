6ee83c31e58fc5562f08116a73cc4f3330a2a6f3
odaiTanan <143601722+odaiTanan@users.noreply.github.com>
2025-03-10 23:11:37 +0300

    Update products-slice.jsx

--- src/rtk/slices/products-slice.jsx
+++ src/rtk/slices/products-slice.jsx
@@ -5,8 +5,10 @@ export const fetchedProducts = createAsyncThunk(
   "productsSlice/fetchedProducts",
   async (baseUrl) => {
     const baseUr = baseUrl;
-    const res = await axios.get(`${baseUr}show/menu.php`);
-    return res.data;
+    const res = await fetch(`${baseUr}show/menu.php`);
+    const jsonData=await res.json();
+    const data= await jsonData.data;
+    return data;
   }
 );
 const productsSlice = createSlice({
