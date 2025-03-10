052827e1da130112ebf5a1d52ac97eeafe0b4d6f
odaiTanan <143601722+odaiTanan@users.noreply.github.com>
2025-03-11 01:49:28 +0300

    Update configure-slice.jsx

--- src/rtk/slices/configure-slice.jsx
+++ src/rtk/slices/configure-slice.jsx
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const configureSlice = createSlice({
-  initialState: "http://odaidelivery.atwebpages.com/",
+  initialState: "/.netlify/functions/proxy/",
   name: "configureSlice",
   reducers: {},
 });
