f84060fdf2383ecea27af37673f18b34b9019134
odaiTanan <143601722+odaiTanan@users.noreply.github.com>
2025-03-11 01:02:53 +0300

    Add files via upload

---
+++ functions/proxy.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+  const path = event.path.replace('/.netlify/functions/proxy', '');
+  const baseUrl = 'http://odaidelivery.atwebpages.com';
+  const url = `${baseUrl}${path}`;
+
+  try {
+    const response = await axios({
+      method: event.httpMethod,
+      url: url,
+      data: event.body,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return {
+      statusCode: response.status,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: error.response ? error.response.status : 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
