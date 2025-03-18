2375ab34c895b3c44ccf9ed7192d2a1ceac45aae
odaiTanan <143601722+odaiTanan@users.noreply.github.com>
2025-03-18 22:51:10 +0300

    Update configure-slice.jsx

--- src/rtk/slices/configure-slice.jsx
+++ src/rtk/slices/configure-slice.jsx
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const configureSlice = createSlice({
-  initialState: "http://odaidelivery.atwebpages.com/",
+  initialState: "https://odaidelivery.atwebpages.com/",
   name: "configureSlice",
   reducers: {},
 });
