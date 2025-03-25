d7d8e521d2ce2a86f905481ac33a87f0f8de952d
odaiTanan <143601722+odaiTanan@users.noreply.github.com>
2025-03-26 01:04:44 +0300

    Update RequiredAuth.jsx

--- src/RequiredAuth.jsx
+++ src/RequiredAuth.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
-import Cookies from "universal-cookie";
+
 const RequiredAuth = () => {
-  const cookie=new Cookies();
-  const user = useSelector((state) => state.user?.user?.token)| cookie.get("token");
+ 
+  const user = useSelector((state) => state.user?.user?.token)
   //required authantication for many pages
   return user ? <Outlet /> : <Navigate to="/auth/signIn" />;
 };
