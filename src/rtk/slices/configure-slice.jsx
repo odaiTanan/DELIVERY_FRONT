4107e0579e9377c2693d9988f8ed096ced32a311
odaiTanan <143601722+odaiTanan@users.noreply.github.com>
2025-03-25 20:47:16 +0300

    Update configure-slice.jsx

--- src/rtk/slices/configure-slice.jsx
+++ src/rtk/slices/configure-slice.jsx
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const configureSlice = createSlice({
-  initialState: "https://corsproxy.io/http://deliveryodai.mooo.com/",
+  //initialState: "https://corsproxy.io/http://deliveryodai.mooo.com/",
+  initialState: "http://localhost/resturantpost/",
   name: "configureSlice",
   reducers: {},
 });
